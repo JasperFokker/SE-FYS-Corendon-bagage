389a6575b723c585f2b40f2cfdf16ab0e6b087da
JasperFokker <jasperfokker@hotmail.com>
2016-10-14 15:09:37 +0200

    Re-organized & Template

---
+++ src/sefyscorendon/Floris.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sefyscorendon;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jimmy
+ */
+public class Floris extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+        returnScherm();
+    }
+    
+    public static GridPane returnScherm() {
+        GridPane scherm = new GridPane();
+        scherm.setPrefSize(600, 450);
+        
+        /* 
+        +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        =====================================================================
+        HIERONDER WERK JE!!
+        HIERONDER WERK JE!!
+        +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        =====================================================================
+        */
+        
+        
+        
+        Label label = new Label();
+        label.setText("Hello World");
+        
+        
+        Button btn = new Button();
+        btn.setText("Hello World");
+        btn.setPrefSize(300, 20);
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                //event handle voor de button
+                System.out.println("Hello World");
+            }
+        });
+                    
+        scherm.add(label, 0, 0);
+        scherm.add(btn, 0, 1);
+        
+        
+        
+        
+        
+        /* 
+        +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        =====================================================================
+        HIERONDER WERK JE NIET!!
+        HIERONDER WERK JE NIET!!
+        +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        =====================================================================
+        */
+        return scherm;
+    }
+}
---
+++ src/sefyscorendon/SeFYSCORENDON.java
@@ -0,0 +1,108 @@
+package sefyscorendon;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author TEAM TWEE
+ */
+public class SeFYSCORENDON extends Application
+{
+    
+    
+    
+    @Override
+    public void start(Stage primaryStage)
+    {
+        BorderPane ui = new BorderPane();
+        VBox menu = new VBox();
+        GridPane rick = Rick.returnScherm();
+        GridPane jason = Rick.returnScherm();
+        GridPane thom = Rick.returnScherm();
+        GridPane floris = Rick.returnScherm();
+        
+        
+        Button btn1 = new Button();
+        btn1.setText("UITLOGGEN");
+        btn1.setPrefSize(200, 90);
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                //event handle voor de button
+                ui.setCenter(rick);
+            }
+        });
+        
+        Button btn2 = new Button();
+        btn2.setText("RICK");
+        btn2.setPrefSize(200, 90);
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                //event handle voor de button
+                ui.setCenter(rick);
+            }
+        });
+        
+        Button btn3 = new Button();
+        btn3.setText("JASON");
+        btn3.setPrefSize(200, 90);
+        btn3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                //event handle voor de button
+                ui.setCenter(jason);
+            }
+        });
+        
+        Button btn4 = new Button();
+        btn4.setText("THOM");
+        btn4.setPrefSize(200, 90);
+        btn4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                //event handle voor de button
+                ui.setCenter(thom);
+            }
+        });
+        
+        Button btn5 = new Button();
+        btn5.setText("FLORIS");
+        btn5.setPrefSize(200, 90);
+        btn5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                //event handle voor de button
+                ui.setCenter(floris);
+            }
+        });
+        
+        
+        menu.getChildren().addAll(btn1,btn2,btn3,btn4,btn5);
+        
+        ui.setLeft(menu);
+        
+        
+        
+        Scene scene = new Scene(ui, 800, 450);
+        
+        primaryStage.setTitle("Main Screen");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+   
+    public static void main(String[] args)
+    {
+        launch(args);
+    }    
+}
