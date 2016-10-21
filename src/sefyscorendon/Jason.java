57f80f841d4d5c4fa46229189c4c61fdc7aec3f9
JasperFokker <jasperfokker@hotmail.com>
2016-10-21 22:24:46 +0200

    Beta1

--- src/sefyscorendon/Jason.java
+++ src/sefyscorendon/Jason.java
@@ -43,26 +43,31 @@ public class Jason extends Application
         Label label = new Label();
         label.setText("Instellingen");
         
+        Label text1 = new Label();
+        text1.setText("Vul het gewicht afgerond in in kilogrammen");
+        
+        Label text2 = new Label();
+        text2.setText("Dit geeft niet, dit kan gewoon leeg gelaten worden.");
         
         Button btn = new Button();
-        btn.setText("Taal:");
+        btn.setText("Kan het gewicht niet invoeren.");
         btn.setPrefSize(300, 20);
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override 
             public void handle(ActionEvent event) {
-                //event handle voor de button
-                System.out.println("Taal:");
+                scherm.add(text1,0,10);
+                
             }
         });
         
        
         Button btn2 = new Button();
-        btn2.setText("Achtergrondkleur:");
+        btn2.setText("De koffer heeft geen merk.");
         btn2.setPrefSize(300, 20);
         btn2.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Achtergrondkleur:");
+                scherm.add(text2,0,10);
             }
         });
                     
