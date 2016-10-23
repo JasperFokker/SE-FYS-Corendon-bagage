d913237ac37b07dedf32766ebe20b623bfbaca13
Jimmy Egberts <jimmyegberts@hotmail.com>
2016-10-23 17:42:06 +0200

    FINAL BUILD

--- src/sefyscorendon/WelkomScherm.java
+++ src/sefyscorendon/WelkomScherm.java
@@ -25,7 +25,7 @@ public class WelkomScherm extends Application {
         scherm.setPrefSize(600, 450);
         scherm.setHgap(10);
         scherm.setVgap(10);
-        scherm.setPadding(new Insets(25, 25, 25, 25));
+        scherm.setPadding(new Insets(50, 25, 25, 25));
 
         Text welkombericht1 = new Text();
         welkombericht1.setText("Welkom bij het");
@@ -36,7 +36,7 @@ public class WelkomScherm extends Application {
         welkombericht2.setFont(Font.font("Verdana", 36));
         
         scherm.add(welkombericht1, 1, 10);
-        scherm.add(welkombericht2, 1, 14);
+        scherm.add(welkombericht2, 1, 12);
 
         return scherm;
     }
